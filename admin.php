dc13f4eaffafc2f2f3fca438d2fd70da4e05083c
naiaero <salsabilanailafahdi26@gmail.com>
2025-09-20 22:37:55 +0800

    baru sebagian dashboard adminnya

---
+++ admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+    <link rel="stylesheet" href="assets/css/admin.css">
+</head>
+<body class="admin">
+    <div style="display: flex; flex-direction: row;">
+        <div>
+            <img src="assets/img/menu-close.png" class="admin-header2">
+        </div>
+        <div>
+            <h1 class="admin-header">Admin Dashboard's</h1>
+        </div>
+        
+    </div>
+    
+    <div class="garis-hori"></div>
+
+    <div style="display: flex; flex-direction: row; place-content: center; text-align: center">
+        <div style="width: 30%;">
+            <img src="assets/img/home.png" class="admin-nav kotak">
+        </div>
+        <div style="width: 40%;">
+            <img src="assets/img/mail-open.png" class="admin-nav kotak">
+        </div>
+        <div style="width: 30%;">
+            <img src="assets/img/car.png" class="admin-nav kotak">
+        </div>
+    </div>
+</body>
+</html>
