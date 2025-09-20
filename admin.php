87d748a14a5d0718835959253ad17f521a751680
naiaero <salsabilanailafahdi26@gmail.com>
2025-09-21 06:44:04 +0800

    lanjutkan pls

--- admin.php
+++ admin.php
@@ -21,13 +21,38 @@
 
     <div style="display: flex; flex-direction: row; place-content: center; text-align: center">
         <div style="width: 30%;">
-            <img src="assets/img/home.png" class="admin-nav kotak">
+            <img src="assets/img/plus-square.png" class="admin-nav kotak">
+            <br>
+            <button class="btn-create">Tambah Mobil</button>
         </div>
         <div style="width: 40%;">
             <img src="assets/img/mail-open.png" class="admin-nav kotak">
+            <br>
+            <button class="btn-create">Lihat Pesan</button>
         </div>
         <div style="width: 30%;">
             <img src="assets/img/car.png" class="admin-nav kotak">
+            <br>
+            <button class="btn-create">Lihat Mobil</button>
+        </div>
+    </div>
+
+    <div style="display: flex; flex-direction: row; place-content: center; text-align: center; margin-top: 20px">
+        <div style="width: 30%;">
+        </div>
+        <div style="width: 25%;">
+            <img src="assets/img/backspace.png" class="admin-nav kotak">
+            <br>
+            <button class="btn-create">Hapus Mobil</button>
+        </div>
+        <div style="width: 10%;">
+        </div>
+        <div style="width: 25%;">
+            <img src="assets/img/edit.png" class="admin-nav kotak">
+            <br>
+            <button class="btn-create">Update Mobil</button>
+        </div>
+        <div style="width: 30%;">
         </div>
     </div>
 </body>
