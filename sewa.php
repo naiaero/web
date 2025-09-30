c5f7209fd09ba6fbfdab7124a01e4230a19425f8
naiaero <salsabilanailafahdi26@gmail.com>
2025-09-30 12:01:50 +0800

    jangan luupa pulll

--- sewa.php
+++ sewa.php
@@ -3,9 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <title>Rental Mobil</title>
+  <link rel="stylesheet" href="assets/css/sewa.css">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body class="body"> 
+<body class="body-sewa">
   <!-- Header -->
   <table width="100%" cellpadding="0" cellspacing="0" style="margin-bottom:32px;">
     <tr>
@@ -13,7 +14,7 @@
       <td align="right" style="padding:24px 32px 16px 0;">
         <span style="font-family:sans-serif;font-size:14px;">
           <a href="index.php" style="margin-right:24px;text-decoration:none;color:#222;">Tentang Kami</a>
-          <button class="btn-pes"" onclick="location.href='index.php'>Logout</button>
+          <button class="btn-pes"" onclick="location.href='index.php'">Logout</button>
         </span>
       </td>
     </tr>
